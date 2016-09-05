14512dd6e237b3099dfdf91f1813a01e6a1d55af
Jeremy LEFEBVRE--AGNES <student42@yopmail.com>
2016-09-05 13:23:10 +0200

    ex08 ex09 d05 piscine c 42

--- d05/ex09/main.c
+++ d05/ex09/main.c
@@ -6,18 +6,17 @@
 /*   By: student42 <student42@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/05 12:53:04 by student42         #+#    #+#             */
-/*   Updated: 2016/09/05 13:21:55 by student42        ###   ########.fr       */
+/*   Updated: 2016/09/05 13:21:58 by student42        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strupcase(char *str)
 {
-
-	if (str[0] >= 0141 && str[0] <= 0x7a)
-		str[0] -= 0x20;
+	if (str[0] >= 0x41 && str[0] <= 90)
+		str[0] += 32;
 	for (int i = 1; str[i]; ++i){
-		if (str[i] >= 0x61 && str[i] <= 0172)
-			str[i] -= 040;
+		if (str[i] >= 0101 && str[i] <= 0x5a)
+			str[i] += 040;
 	}
 	return (str);
 }
@@ -26,8 +25,8 @@ char	*ft_strupcase(char *str)
 
 int		main(void)
 {
-	printf("%s\n", ft_strupcase(strdup("hello world")));
-	printf("%s\n", ft_strupcase(strdup("hello world ! 0123456789 Swag\tyolo NewMot\vmot")));
+	printf("%s\n", ft_strupcase(strdup("Hello World")));
+	printf("%s\n", ft_strupcase(strdup("Hello world ! 0123456789 Swag\tyolo NewMot\vmot")));
 	printf("%s\n", ft_strupcase(strdup("VOUS SUCEZ DES BITES !")));
 	printf("%s\n", ft_strupcase(strdup("vous sucez des bites !")));
 	return (0);
