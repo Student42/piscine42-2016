9e322b030031a965f7bad9feb0157b60381497e9
Jeremy LEFEBVRE--AGNES <student42@yopmail.com>
2016-09-05 13:29:50 +0200

    ex098 ex10 d05 piscine c 42

--- d05/ex10/main.c
+++ d05/ex10/main.c
@@ -5,29 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: student42 <student42@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/05 12:53:04 by student42         #+#    #+#             */
-/*   Updated: 2016/09/05 13:28:55 by student42        ###   ########.fr       */
+/*   Created: 2016/09/05 13:24:11 by student42         #+#    #+#             */
+/*   Updated: 2016/09/05 13:29:08 by student42        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strlowercase(char *str)
+char	*ft_strcapitalize(char *str)
 {
-	if (str[0] >= 0x41 && str[0] <= 90)
-		str[0] += 32;
+	for (int i = 0; str[i]; ++i){ if (str[i] >= 0101 && str[i] <= 0x5a)
+		str[i] += 32;
+	}
+	if (str[0] >= 0141 && str[0] <= 0x7a)
+		str[0] -= 0x20;
 	for (int i = 1; str[i]; ++i){
-		if (str[i] >= 0101 && str[i] <= 0x5a)
-			str[i] += 040;
+		if (str[i] >= 0x61 && str[i] <= 0172 &&
+				(str[i - 1] == ' ' || (str[i - 1] > 8 && str[i - 1] < 14)))
+			str[i] -= 040;
 	}
 	return (str);
 }
 
-#include <string.h>
-
 int		main(void)
 {
-	printf("%s\n", ft_strlowercase(strdup("Hello World")));
-	printf("%s\n", ft_strlowercase(strdup("Hello world ! 0123456789 Swag\tyolo NewMot\vmot")));
-	printf("%s\n", ft_strlowercase(strdup("VOUS SUCEZ DES BITES !")));
-	printf("%s\n", ft_strlowercase(strdup("vous sucez des bites !")));
+	printf("%s\n", ft_strcapitalize(strdup("hello world")));
+	printf("%s\n", ft_strcapitalize(strdup("hello world ! 0123456789 Swag\tyolo NewMot\vmot")));
+	printf("%s\n", ft_strcapitalize(strdup("VOUS SUCEZ DES BITES !")));
+	printf("%s\n", ft_strcapitalize(strdup("vous sucez des bites !")));
 	return (0);
 }
