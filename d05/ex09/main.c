781f07849cdf951ef6ab6bb7ebb580f9db54b377
Jeremy LEFEBVRE--AGNES <student42@yopmail.com>
2016-09-05 13:32:22 +0200

    ex09 ex10 d05 piscine c 42

--- d05/ex09/main.c
+++ d05/ex09/main.c
@@ -6,11 +6,11 @@
 /*   By: student42 <student42@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/05 12:53:04 by student42         #+#    #+#             */
-/*   Updated: 2016/09/05 13:28:55 by student42        ###   ########.fr       */
+/*   Updated: 2016/09/05 13:32:00 by student42        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strlowercase(char *str)
+char *ft_strlowcase(char *str)
 {
 	if (str[0] >= 0x41 && str[0] <= 90)
 		str[0] += 32;
@@ -25,9 +25,9 @@ char	*ft_strlowercase(char *str)
 
 int		main(void)
 {
-	printf("%s\n", ft_strlowercase(strdup("Hello World")));
-	printf("%s\n", ft_strlowercase(strdup("Hello world ! 0123456789 Swag\tyolo NewMot\vmot")));
-	printf("%s\n", ft_strlowercase(strdup("VOUS SUCEZ DES BITES !")));
-	printf("%s\n", ft_strlowercase(strdup("vous sucez des bites !")));
+	printf("%s\n", ft_strlowcase(strdup("Hello World")));
+	printf("%s\n", ft_strlowcase(strdup("Hello world ! 0123456789 Swag\tyolo NewMot\vmot")));
+	printf("%s\n", ft_strlowcase(strdup("VOUS SUCEZ DES BITES !")));
+	printf("%s\n", ft_strlowcase(strdup("vous sucez des bites !")));
 	return (0);
 }
